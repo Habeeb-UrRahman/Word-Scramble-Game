4582492b241aa51dcb52145b21794b7212cfbb7d
Habeeb Ur Rahman <rahman.habeeb@outlook.com>
2023-09-07 13:08:50 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,294 @@
+let words = [
+    {
+        word: "addition",
+        hint: "The process of adding numbers"
+    },
+    {
+        word: "meeting",
+        hint: "Event in which people come together"
+    },
+    {
+        word: "number",
+        hint: "Math symbol used for counting"
+    },
+    {
+        word: "exchange",
+        hint: "The act of trading"
+    },
+    {
+        word: "canvas",
+        hint: "Piece of fabric for oil painting"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "position",
+        hint: "Location of someone or something"
+    },
+    {
+        word: "feather",
+        hint: "Hair like outer covering of bird"
+    },
+    {
+        word: "comfort",
+        hint: "A pleasant feeling of relaxation"
+    },
+    {
+        word: "tongue",
+        hint: "The muscular organ of mouth"
+    },
+    {
+        word: "expansion",
+        hint: "The process of increase or grow"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "store",
+        hint: "Large shop where goods are traded"
+    },
+    {
+        word: "field",
+        hint: "Area of land for farming activities"
+    },
+    {
+        word: "friend",
+        hint: "Person other than a family member"
+    },
+    {
+        word: "pocket",
+        hint: "A bag for carrying small items"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "expert",
+        hint: "Person with extensive knowledge"
+    },
+    {
+        word: "statement",
+        hint: "A declaration of something"
+    },
+    {
+        word: "second",
+        hint: "One-sixtieth of a minute"
+    },
+    {
+        word: "library",
+        hint: "Place containing collection of books"
+    },
+    {
+        word: "elephant",
+        hint: "Large mammal with a trunk"
+    },
+    {
+        word: "keyboard",
+        hint: "Input device for computers"
+    },
+    {
+        word: "oxygen",
+        hint: "Essential gas for breathing"
+    },
+    {
+        word: "mountain",
+        hint: "Tall landform with peaks"
+    },
+    {
+        word: "diamond",
+        hint: "Precious gemstone"
+    },
+    {
+        word: "language",
+        hint: "System of communication"
+    },
+    {
+        word: "umbrella",
+        hint: "Rain protection device"
+    },
+    {
+        word: "chocolate",
+        hint: "Sweet treat made from cocoa"
+    },
+    {
+        word: "sunshine",
+        hint: "Bright daylight"
+    },
+    {
+        word: "freedom",
+        hint: "The state of being free"
+    },
+    {
+        word: "universe",
+        hint: "All of space and everything in it"
+    },
+    {
+        word: "butterfly",
+        hint: "Colorful insect with wings"
+    },
+    {
+        word: "whisper",
+        hint: "Speak softly"
+    },
+    {
+        word: "laughter",
+        hint: "Sound of amusement"
+    },
+    {
+        word: "guitar",
+        hint: "Musical instrument with strings"
+    },
+    {
+        word: "swimming",
+        hint: "Activity in water"
+    },
+    {
+        word: "shoes",
+        hint: "Footwear"
+    },
+    {
+        word: "camera",
+        hint: "Device for taking photos"
+    },
+    {
+        word: "island",
+        hint: "Land surrounded by water"
+    },
+    {
+        word: "flower",
+        hint: "Blossoming plant"
+    },
+    {
+        word: "puzzle",
+        hint: "A challenging problem or game"
+    },
+    {
+        word: "happiness",
+        hint: "Feeling of joy"
+    },
+    {
+        word: "bicycle",
+        hint: "Two-wheeled vehicle"
+    },
+    {
+        word: "moonlight",
+        hint: "Light from the moon"
+    },
+    {
+        word: "history",
+        hint: "Study of the past"
+    },
+    {
+        word: "holiday",
+        hint: "A day of celebration"
+    },
+    {
+        word: "ocean",
+        hint: "Vast body of saltwater"
+    },
+    {
+        word: "wonderful",
+        hint: "Extremely good or impressive"
+    },
+    {
+        word: "squirrel",
+        hint: "Small, furry rodent"
+    },
+    {
+        word: "crystal",
+        hint: "Transparent mineral"
+    },
+    {
+        word: "adventure",
+        hint: "Exciting journey"
+    },
+    {
+        word: "happiness",
+        hint: "Feeling of joy"
+    },
+    {
+        word: "breeze",
+        hint: "Gentle wind"
+    },
+    {
+        word: "fantastic",
+        hint: "Extraordinarily good"
+    },
+    {
+        word: "candle",
+        hint: "Wax stick for lighting"
+    },
+    {
+        word: "fountain",
+        hint: "Water feature"
+    },
+    {
+        word: "laughter",
+        hint: "Sound of amusement"
+    },
+    {
+        word: "journey",
+        hint: "Travel or passage"
+    },
+    {
+        word: "sunrise",
+        hint: "Morning dawn"
+    },
+    {
+        word: "paradise",
+        hint: "Heavenly place"
+    },
+    {
+        word: "sweater",
+        hint: "Warm clothing item"
+    },
+    {
+        word: "blossom",
+        hint: "Flower in bloom"
+    },
+    {
+        word: "moonlight",
+        hint: "Light from the moon"
+    },
+    {
+        word: "astronomy",
+        hint: "Study of celestial objects"
+    },
+    {
+        word: "garden",
+        hint: "Outdoor space for plants"
+    },
+    {
+        word: "chocolate",
+        hint: "Sweet treat made from cocoa"
+    },
+    {
+        word: "sparkle",
+        hint: "Shine with small, bright flashes"
+    },
+    {
+        word: "silence",
+        hint: "Absence of sound"
+    },
+    {
+        word: "puzzle",
+        hint: "A challenging problem or game"
+    },
+    {
+        word: "jungle",
+        hint: "Tropical forest with dense vegetation"
+    },
+]
